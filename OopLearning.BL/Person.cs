fd9aacf7554b986160a569ac988742615275289f
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-05-27 11:36:59 +0200

    Created a method for Validating the name and used it in the Name property Set.

---
+++ OopLearning.BL/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace OopLearning.BL
+{
+    public class Person
+    {
+        private string name;
+        private string cpr;
+
+        public string Cpr { get => cpr; set => cpr = value; }
+        public DateTime Birthday {
+            get
+            {
+                
+                return Birthday;
+            }
+        }
+        public bool IsWoman { get; }
+
+        public Person ()
+        {
+
+        }
+        public Person(string name, string cpr)
+        {
+            Name = name;
+            Cpr = cpr;
+        }
+
+        public string Name {
+            get => name;
+            set
+            {
+                var nameCheck = ValidateName(value);
+                if (nameCheck.IsValid)
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new ArgumentException(nameCheck.errMsg, nameof(name));
+                }
+            }
+        }
+
+        public static (bool IsValid, string errMsg) ValidateName(string name)
+        {
+            if (name is null)
+            {
+                return (false, "Navnet er tomt. Husk og udfyld det");
+            }
+
+            name = name.Trim();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return (false, "Navnt består kun af white spaces");
+            }
+
+            if (!name.Contains(' '))
+            {
+                return (false, "Du ser ud til ikke og havde et efternavn, husk at udfylde det fulde navn");
+            }
+
+            if (name.Length < 1)
+            {
+                return (false, "Navnet skal være mere end 1 bogstav langt");
+            }
+
+            return (true, "");
+        }
+
+
+    }
+}
