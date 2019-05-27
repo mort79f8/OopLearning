bb2b1d578ffed0fd41e0e394fde9933f53d4a444
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-05-27 14:35:07 +0200

    finished Field class

--- OopLearning.BL/field.cs
+++ OopLearning.BL/field.cs
@@ -45,8 +45,38 @@ namespace OopLearning.BL
                 var cropCheck = ValidateCrop(value);
             }
         }
-        public double Area { get; }
-        public double Yield { get; }
+        public double Area {
+            get
+            {
+                if (Length == 0 || Width == 0)
+                {
+                    throw new InvalidOperationException("Length eller Width må ikke være 0");
+                }
+                else
+                {
+                    return Length*Width;
+                }
+            }
+        }
+        public double Yield {
+            get
+            {
+                switch (Crop)
+                {
+                    case "Wheat":
+                        return (Area / 100) * 2;
+                    case "Potatoes":
+                        return (Area / 100) * 4;
+                    case "Oat":
+                        return (Area / 200) * 3;
+                    case "Carrots":
+                        return (Area / 30) * 3;
+                    default:
+                        return 0;
+                }
+
+            }
+        }
 
         public Field()
         {
@@ -60,6 +90,24 @@ namespace OopLearning.BL
             Area = area;
             Yield = yield;
         }
+
+        public static (bool IsValid, string errMessage) ValidateCrop(string value)
+        {
+            if (value is null)
+            {
+                return (false, "length eller width er 0");
+            }
+
+            if (value == "Potatoes" || value == "Wheat" || value == "Oat" || value == "Carrots")
+            {
+                return (true, "");
+            }
+            else
+            {
+                return (false, "Der må kun være Potatoes, Wheat, Oak eller Carrots på marken");
+            }
+        }
+
 
 
     }
