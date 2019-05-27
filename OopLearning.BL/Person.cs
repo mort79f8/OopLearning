86ef3f1e20bdd8f7d1bed86dae1a20005fdf7c31
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-05-27 13:34:13 +0200

    Finished the Person class

--- OopLearning.BL/Person.cs
+++ OopLearning.BL/Person.cs
@@ -7,15 +7,41 @@ namespace OopLearning.BL
         private string name;
         private string cpr;
 
-        public string Cpr { get => cpr; set => cpr = value; }
+        public string Cpr {
+            get => cpr;
+            set
+            {
+                var cprCheck = ValidateCpr(value);
+                if (cprCheck.isValid)
+                {
+                    cpr = value;
+                }
+                else
+                {
+                    throw new ArgumentException(cprCheck.errMessage, nameof(cpr));
+                }
+            }
+        }
         public DateTime Birthday {
             get
             {
-                
-                return Birthday;
+                DateTime.TryParse(Cpr.Substring(0, 2) + "-" + Cpr.Substring(2, 2) + "-" + Cpr.Substring(4, 2), out DateTime convertedCpr);
+                return convertedCpr;
             }
         }
-        public bool IsWoman { get; }
+        public bool IsWoman {
+            get
+            {
+                if ((int.Parse(Cpr.Substring(9,1)) % 2) > 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
 
         public Person ()
         {
@@ -69,6 +95,36 @@ namespace OopLearning.BL
             return (true, "");
         }
 
+        public static (bool isValid, string errMessage) ValidateCpr(string cpr)
+        {
+            if (cpr is null)
+            {
+                return (false, "cpr er tomt, husk og udfyld den");
+            }
+            
+            if (cpr.Length != 10)
+            {
+                return (false, "cpr er ikke udfylt korrekt, formatet skal være dagmånedårdesidstefire eks: 2108765785");
+            }
+
+            if (int.Parse(cpr.Substring(0,2)) < 1 || int.Parse(cpr.Substring(2, 2)) > 31)
+            {
+                return (false, "de 4 første tal er ikke en gyldig fødselsdato");
+
+            }
+
+            if (int.Parse(cpr.Substring(4, 2)) < 0 || int.Parse(cpr.Substring(4, 2)) > 99)
+            {
+                return (false, "års talet er ikke en gyldig fødselsdato");
+            }
+
+            if (DateTime.Parse(cpr.Substring(0, 2) + "-" + cpr.Substring(2, 2) + "-" + cpr.Substring(4, 2)) > DateTime.Today)
+            {
+                return (false, "det cpr er ude i fremtiden, det dutter ikke");
+            }
+
+            return (true, "");
+        }
 
     }
 }
---
+++ TestLab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OopLearning.BL;
+
+namespace TestLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Morten Juul";
+            Console.WriteLine(person.Name);
+            person.Cpr = "2108761353";
+            Console.WriteLine(person.Cpr);
+            Console.WriteLine(person.Birthday.ToShortDateString());
+            Console.WriteLine(person.IsWoman);
+        }
+    }
+}
