58f959756b65b626140174682e25ffc0e8d50a23
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-05-29 10:06:10 +0200

    Created new project in the OopLearning solution. learning about inherance added Abstract Class CustomFileInto added Class DocumentFileInfo who inherit from CustomeFileInfo

---
+++ OopLearning.BL/Inheritance/CustomFileInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopLearning.BL.Inheritance
+{
+    abstract class CustomFileInfo
+    {
+        protected string fileName;
+        protected int fileSize;
+        protected DateTime creationTime;
+
+        protected CustomFileInfo(string name, int size, DateTime creation)
+        {
+            fileName = name;
+            fileSize = size;
+            creationTime = creation;
+        }
+
+        public bool IsSizeTooLarge()
+        {
+            return "not definded yet"
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ OopLearning.BL/Inheritance/DocumentFileInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopLearning.BL.Inheritance
+{
+    class DocumentFileInfo : CustomFileInfo
+    {
+        public DocumentFileInfo(string name, int size, DateTime creation) : base(name, size, creation)
+        {
+        }
+    }
+}
